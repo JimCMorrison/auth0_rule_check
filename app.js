c3596b3033ade108f23c8372d9041b4d25ba501c
JimCMorrison <Jim@CodingMorrison.com>
2018-05-13 09:47:11 -0700

    Updated App.js for better V2 functionalty and auth

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 require('dotenv/config');
 const request = require('request');
-
 const express = require('express');
-
-const pug = require('pug');
-
+const expressSession = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const { ManagementClient } = require('auth0');
+const auth = require('./auth').default;
 
 const app = express();
 const port = 4000;
 
+// View Settings
+app.set('view engine', 'pug');
 app.use(express.json());
+
+// Parser Settings
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Auth Settings
+app.use(expressSession({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(auth.initialize());
+app.use(auth.session());
 
 app.use((req, res, next) => {
   const options = {
@@ -21,33 +36,74 @@ app.use((req, res, next) => {
       client_id: process.env.CLIENT_ID,
       client_secret: process.env.CLIENT_SECRET,
       audience: `https://${process.env.DOMAIN}/api/v2/`,
-      grant_type: 'client_credentials',
+      grant_type: 'client_credentials'
     },
-    json: true,
+    json: true
   };
-  console.log(options, req.path);
+
   request(options, (error, response, body) => {
     if (error) return next(error);
     req.token = body.access_token;
+
+    const management = new ManagementClient({
+      token: req.token,
+      domain: process.env.DOMAIN
+    });
+
+    req.managementApi = management;
     next();
   });
 });
 
-
-app.get('/', (req, res) => {
-  console.log(req.token);
-  const management = new ManagementClient({
-    token: req.token,
-    domain: process.env.DOMAIN,
-  });
-
-  console.log('Here Comes The Rules');
-  management.getRules().then(rules => console.log(rules));
-  console.log('Here Are The Clients');
-  management.getClients().then(clients => console.log(clients));
-  res.send('Test');
+app.use((req, res, next) => {
+  Promise.all([req.managementApi.getRules(), req.managementApi.getClients()])
+    .then(([rules, clients]) => {
+      req.rules = rules;
+      req.clients = clients;
+      next();
+    })
+    .catch(err => next(err));
 });
 
+app.get('/', (req, res) => {
+  if (!req.user) {
+    return res.redirect('/login');
+  }
+
+  return res.render('./index', { rules: req.rules, clients: req.clients });
+});
+
+app.get(
+  '/login',
+  auth.authenticate('auth0', {})
+);
+
+
+app.get(
+  '/callback',
+  auth.authenticate('auth0', { failureRedirect: '/access-denied' }),
+  (req, res, next) => {
+    if (!req.user) {
+      next(new Error('user null'));
+    }
+    res.redirect('/');
+  }
+);
+
+app.get(
+  '/access-denied',
+  (req, res) => {
+    res.send('Contact the Administrator for Access');
+  }
+);
+
+app.get(
+  '/logout',
+  (req, res) => {
+    req.logout();
+    res.redirect('/');
+  }
+);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
